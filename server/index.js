291f8bdac4d71072b7f5c17879deacc95e6a11d2
khifo <khifo09@yahoo.com>
2023-01-16 18:53:13 -0700

    added put, delete requests to goal feature

--- server/index.js
+++ server/index.js
@@ -24,6 +24,22 @@ app.post('/submit-goal', (req, res) => {
     res.status(200).send({ message: 'Goal submitted successfully' });
 });
 
+app.put('/update-goal/:id', (req, res) => {
+    const { id } = req.params;
+    const { goal, deadline } = req.body;
+
+    if (!goal || !deadline) {
+        return res.status(400).send({ error: 'Missing goal or deadline' });
+    }
+     res.status(200).send({ message: 'Goal updated successfully' });
+});
+
+app.delete('/delete-goal/:id', (req, res) => {
+    const { id } = req.params;
+    
+    res.status(200).send({ message: 'Goal deleted successfully' });
+});
+
 
 
 app.listen(4000, () => console.log("Server running on 4000"));
